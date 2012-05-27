626b3996be86d18b555a7a321204f27ac8f28081
David Celis <david@davidcelis.com>
2012-05-27 13:31:26 -0700

    Routes for actions

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,3 +1,5 @@
 class Attribute < ActiveRecord::Base
+  set_inheritance_column :something_other_than_type
+
   belongs_to :action
 end
--- db/migrate/20120414021528_create_scene_attributes.rb
+++ db/migrate/20120414021528_create_scene_attributes.rb
@@ -1,13 +1,14 @@
 class CreateSceneAttributes < ActiveRecord::Migration
   def change
     create_table :scene_attributes do |t|
-      t.string :value # TODO: actually a string?
+      t.string     :value
       t.references :keyframe
       t.references :attribute
       t.references :action_group
 
       t.timestamps
     end
+    
     add_index :scene_attributes, :keyframe_id
     add_index :scene_attributes, :attribute_id
     add_index :scene_attributes, :action_group_id
