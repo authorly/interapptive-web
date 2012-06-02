abcfd6d1fb01acd0c8077615d701ce0271f5c231
Ryan Williams <ryan@wigg.ly>
2012-06-02 19:38:45 +1200

    Monkey patch to fix cocos2d-html5's click event handler

--- app/assets/javascripts/simulator/init.js
+++ app/assets/javascripts/simulator/init.js
@@ -47,7 +47,7 @@ cc.TransitionScene = function () {
 };
 
 window.runSimulator = function () {
-  // Monkey patch to fix bugs in cocos2d-html5
+  // Monkey patch to fix bugs in cocos2d-html5 -- it can't add to DOM correctly
   cc.setupHTML = function(a){
     var b = cc.canvas;
     b.style.zIndex = 0;
@@ -65,6 +65,39 @@ window.runSimulator = function () {
 
     return a
   };
+
+  // Monkey patch to fix bug in touch calculation in cocos2d-html5
+  cc.Touch.prototype.locationInView = function () {
+    var p = this._m_point;
+
+
+    // Ratio of canvas to element size
+    var ratioW = cc.canvas.width / $(cc.canvas).width()
+      , ratioH = cc.canvas.height / $(cc.canvas).height()
+
+    // Fix coords ignoring scroll
+    var scrollV = 0
+      , scrollH = 0
+    var x = $(cc.canvas)
+    while (x.length > 0) {
+      scrollV += x.scrollTop()
+      scrollH += x.scrollLeft()
+
+      x = x.parent()
+    }
+
+    // X coord doesn't consider ratio
+    var realX = (p.x - scrollH) * ratioW
+
+    // Y coord has wrong origin and doesn't consider ratio
+    var realY = (p.y - (cc.canvas.height - $(cc.canvas).height())) - scrollV
+    realY *= ratioH
+
+    var actualPoint = new cc.Point(realX, realY)
+
+    return actualPoint
+  }
+
 
 
   cc.setup("simulator-canvas");
