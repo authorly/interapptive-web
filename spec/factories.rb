1a70018d79ede5ed40e57186e64811f2f49b03cf
Kurt Braget (sword computer) <kurtbraget@gmail.com>
2012-08-08 09:49:08 -0700

    user and storybook specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,3 +2,16 @@ Factory.define :user do |f|
   f.email 'user%d@example.com'
   f.password f.password_confirmation('supersecret')
 end
+
+Factory.define :storybook do |f|
+  f.title 'Test Title'
+  f.author 'Charles Bukowski'
+  f.description 'This book is for true champions'
+  f.publisher 'Black Sparrow Press'
+  f.price 20.00
+  f.record_enabled 'true' # note sure about this attribute. kurt b
+  f.android_or_ios 'android'
+  f.tablet_or_phone 'tablet'
+end
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,27 +1,48 @@
 require 'spec_helper'
 
 describe User do
+  
+  let!(:user) { Factory(:user) }
+  
   describe "authentication" do
     it "should require a unique email" do
       Factory(:user, :email => 'taken@example.com')
-      lambda { Factory(:user, :email => 'taken@example.com') }.should raise_error(ActiveRecord::RecordInvalid)
+      expect { Factory(:user, :email => 'taken@example.com') }.should raise_error(ActiveRecord::RecordInvalid)
     end
 
     it "should require a password and password_confirmation" do
-      lambda { Factory(:user, :password => '') }.should raise_error(ActiveRecord::RecordInvalid)
-      lambda { Factory(:user, :password_confirmation => '') }.should raise_error(ActiveRecord::RecordInvalid)
-
-      lambda { Factory(:user, :password => 'these', :password_confirmation => 'aredifferent') }.should raise_error(ActiveRecord::RecordInvalid)
+      expect { Factory(:user, :password => '') }.should raise_error(ActiveRecord::RecordInvalid)
+      expect { Factory(:user, :password_confirmation => '') }.should raise_error(ActiveRecord::RecordInvalid)
+      expect { Factory(:user, :password => 'these', :password_confirmation => 'aredifferent') }.should raise_error(ActiveRecord::RecordInvalid)
     end
 
     it "should authenticate with a good password" do
-      user = Factory(:user)
       user.authenticate('supersecret').should == user
     end
 
     it "should not authenticate with a bad password" do
-      user = Factory(:user)
       user.authenticate('badpassword').should be_false
     end
+    
+    it "is invalid without email" do 
+      expect { Factory(:user, :email => nil) }.should raise_error(ActiveRecord::RecordInvalid)
+    end
+
   end
+  
+  context "a user (in general)" do
+    it "has a valid factory" do 
+      user.should be_valid
+    end
+    
+    it "is invalid without password"
+    
+    it { should have_many(:storybooks) }
+    it { should have_many(:actions) }
+  end
+  
+  context "developer user should" do 
+    it "be invalid without username"
+  end
+  
 end
