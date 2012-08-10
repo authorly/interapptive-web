e580ba14b096dfaf075ac40d4b8f25f9da285c5d
Waseem Ahmad <talk.to.waseem@gmail.com>
2012-08-11 00:51:04 +0530

    Tests for various assets. Issue #60
    * Tests for Image, Sound, Font and Video assets
    * Mocks out Fog in test environment Issue #61
    * Uses file storage in development environment Issue #61

---
+++ spec/models/image_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Image do
+  let!(:image) { Factory(:image) }
+
+  context "#as_jquery_upload_response" do
+    it 'should be valid response' do
+      # Following chokes with image returned from Factory(:image)
+      im = Image.new
+      im.image = File.open(Rails.root.join("spec/factories/images/350x350.png"))
+      im.save!
+
+      response = {
+        'id'            => im.id,
+        'name'          => im.read_attribute(:image),
+        'size'          => im.image.size,
+        'url'           => im.image.url,
+        'thumbnail_url' => im.image.thumb.url,
+        'delete_url'    => "/images/#{im.id}",
+        'delete_type'   => 'DELETE'
+      }
+
+      im.as_jquery_upload_response.should eql(response)
+    end
+  end
+end
---
+++ spec/support/fog.rb
@@ -0,0 +1,7 @@
+Fog.mock!
+Fog.credentials = {
+  :aws_access_key_id      => 'AKIAJ3N4AG2EGQRMHXRQ',
+  :aws_secret_access_key  => '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'
+}
+connection = Fog::Storage.new(:provider => 'AWS')
+connection.directories.create(:key => 'interapptive')
