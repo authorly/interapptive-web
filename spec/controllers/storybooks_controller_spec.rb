f165a041bc096974fa3a14c115bc9dc31e15f8bb
Chris Whitman <chris@theoryplus.com>
2012-08-10 22:27:25 -0700

    Rename Storybook controller's spec to match others [*_controller_spec.rb]

--- spec/controllers/storybooks_controller_spec.rb
+++ spec/controllers/storybooks_controller_spec.rb
@@ -1,66 +1,44 @@
 require 'spec_helper'
 
-describe StorybooksController, :type => :api do
-  render_views
-
+describe StorybooksController do
   let!(:user) { Factory(:user) }
-
-  before do
-    # request.cookie_jar
-    @request.cookie_jar
-    test_sign_in(user)
-  end
-
+  
   context "authenticated user" do
     it "is authenticated" do
-        get root_url
-        # Only shows sign-in page
-        puts last_response.to_yaml
+      user.authenticate("supersecret").should == user
     end
   end
-
-  # context "authenticated user" do
-  #   it "is authenticated" do
-  #     get root_url
-  #     # Should login via test_sign_in
-  #     # Only renders a sign in form.
-  #     # Possible: dynamic routes (via constraints & cookie) not working?
-  #     #           cookie persistance? See UserSessionsHelper
-  #     puts last_response.to_yaml
-  #   end
-  # end
-  #
-  # describe 'GET #index' do
-  #   it "populates an array of storybooks" do
-  #     user.authenticate "supersecret"
-  #     #user.should be signed in?
-  #     storybook = Factory.create(:storybook)
-  #     get :index, :format => :json
-  #     #assigns(:storybooks).should eq [storybook]
-  #     response.body.should have_content storybook.to_json
-  #     #puts "story book #{storybook}"
-  #     #response.body.should  have_conten storybook
-  #   end
-  #
-  #   it "renders the :index view" do
-  #     user.authenticate "password"
-  #     get :index, :format => :json
-  #     response.should render_template :index
-  #   end
-  # end
-  #
-  # describe 'GET #show' do
-  #   it "assigns the requested storybook to @storybook" do
-  #     storybook = Factory.create(:storybook).attributes
-  #     get :show, id: storybook
-  #     assigns(:storybook).should eq storybook
-  #   end
-  #   it "renders the :show json" do
-  #     storybook = Factory.create(:storybook).attributes
-  #     get :show, id: storybook, :format => :json
-  #     puts "show response #{response.body}"
-  #     response.body.should eq storybook
-  #   end
-  # end
-
+  
+  describe 'GET #index' do
+    it "populates an array of storybooks" do
+      user.authenticate "supersecret"
+      #user.should be signed in?
+      storybook = Factory.create(:storybook)
+      get :index, :format => :json
+      #assigns(:storybooks).should eq [storybook]
+      response.body.should have_content storybook.to_json
+      #puts "story book #{storybook}"
+      #response.body.should  have_conten storybook
+    end
+    
+    it "renders the :index view" do
+      user.authenticate "password"
+      get :index, :format => :json
+      response.should render_template :index
+    end
+  end
+  
+  describe 'GET #show' do
+    it "assigns the requested storybook to @storybook" do
+      storybook = Factory.create(:storybook).attributes
+      get :show, id: storybook 
+      assigns(:storybook).should eq storybook
+    end
+    it "renders the :show json" do 
+      storybook = Factory.create(:storybook).attributes
+      get :show, id: storybook, :format => :json
+      puts "show response #{response.body}"
+      response.body.should eq storybook
+    end
+  end
 end
