95ce4b9c1cf24c499362d2f6dbadaca359996734
Waseem Ahmad <talk.to.waseem@gmail.com>
2012-10-05 03:14:49 +0530

    ISSUE #67 Model and controller tests

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,7 +9,7 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email params[:email]
     user.send_password_reset if user
-    redirect_to sign_in_path, :notice => "Email sent with password reset instructions."
+    redirect_to sign_in_url, :notice => "Email sent with password reset instructions."
   end
   
   # GET /account/password_resets/:id
@@ -22,11 +22,11 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by_password_reset_token!(params[:id])
 
     if @user.password_reset_sent_at < 2.hours.ago
-      redirect_to new_password_reset_path, :alert => "Password reset has expired."
+      redirect_to new_password_reset_url, :alert => "Password reset has expired."
     elsif params[:user][:password].blank? || params[:user][:password_confirmation].blank?
       redirect_to edit_password_reset_url(@user.password_reset_token), :alert => "Can not accept blank password"
     elsif @user.update_attributes(:password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
-      redirect_to root_path, :notice => "Password has been reset!"
+      redirect_to root_url, :notice => "Password has been reset!"
     else
       render :edit
     end
