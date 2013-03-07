cec5a5c2723b1e6fa94e4a14f9aa082f52714740
Chris Whitman <christopherscottwhitman@gmail.com>
2013-03-07 15:37:39 -0800

    Add region to CarrierWave initializer/config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAJ3N4AG2EGQRMHXRQ',
-    :aws_secret_access_key  => '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'
+    :aws_secret_access_key  => 'zonFFwsM1qY1tueduERgYgubfE9yU46KKgju6p78',
+    :region                 => 'us-west-1'
   }
 
   config.fog_directory  = 'interapptive'
