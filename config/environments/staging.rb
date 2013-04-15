caf4aac14075f68a801ac1c8aeba61455102a512
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-04-15 18:38:15 +0530

    Trun on assets debugging in staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -23,6 +23,9 @@ Interapptive::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = false
 
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
