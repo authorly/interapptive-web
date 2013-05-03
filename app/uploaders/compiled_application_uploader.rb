93f8edd57548f82e6433fc3dce52e1df518c9ade
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-05-04 03:27:11 +0530

    ISSUE #473 Make files private.

--- app/uploaders/compiled_application_uploader.rb
+++ app/uploaders/compiled_application_uploader.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 
 class CompiledApplicationUploader < CarrierWave::Uploader::Base
-  #@fog_public = false
-  #@fog_authenticated_url_expiration = 3600 # One hour
+  @fog_public = false
+  @fog_authenticated_url_expiration = 3600 # One hour
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development?
