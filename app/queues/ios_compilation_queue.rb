9637c3ee990e3b5f47e5df8194324ead86c2e09a
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-05-14 21:48:26 +0530

    Disable cleanup of assets for now.

--- app/queues/ios_compilation_queue.rb
+++ app/queues/ios_compilation_queue.rb
@@ -12,7 +12,7 @@ class IosCompilationQueue < GenericQueue
 
     storybook_application.compile
     storybook_application.upload_compiled_application
-    storybook_application.cleanup
+    #storybook_application.cleanup
     storybook_application.send_notification
     storybook_application
   end
