c870e917ef217bab2ddcf5d75aa9c6acd36d08e4
Irina Dumitrascu <me@dira.ro>
2013-08-20 19:34:39 +0300

    Attempt to solve #814
    after noticing that the images delivered through https do not load,
    but they work if requested through http)
    Happened on Chrome, on staging.
    (this happens even if trying to open an image in a separate tab.)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,8 @@ CarrierWave.configure do |config|
   }.merge(Rails.application.config.s3_credentials)
 
   config.fog_directory  = Rails.application.config.s3_bucket
+  # Forcing use of HTTP
+  config.asset_host = "http://#{config.fog_directory}.s3.amazonaws.com"
 end
 
 if Rails.env.test?
