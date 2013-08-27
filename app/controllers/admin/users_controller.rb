57d47801a823b458cc5e433ac5e4ed69b53828cc
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-08-27 06:11:37 +0530

    This is a combination of 3 commits. (#838) Let admin make other users admin and specify allowed storybooks count.
    (#838) Let admin create new users.
    
    (#838) Use a suitable method name for user json.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,40 @@
 module Admin
   class UsersController < Admin::BaseController
     def index
-      @users = User.order('id').page(params[:page]).per(3)
+      @users = User.order('id').page(params[:page]).per(50)
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      params[:user].slice!(:is_admin, :allowed_storybooks_count)
+
+      @user.update_attributes(params[:user])
+      respond_to do |format|
+        format.html { redirect_to :action => :edit }
+      end
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      params[:user].slice!(:email, :is_admin, :allowed_storybooks_count)
+      @user = User.new(params[:user])
+
+      if @user.save_by_admin
+        respond_to do |format|
+          format.html { redirect_to edit_admin_user_url(@user.id) }
+        end
+      else
+        respond_to do |format|
+          format.html { render :action => :new }
+        end
+      end
     end
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,14 @@ class UserMailer < ActionMailer::Base
     mail :to => user.email, :subject => "Password Reset"
   end
 
+  def password_creation_by_admin_notification(user_id, pass)
+    @user = User.find_by_id(user_id)
+    @pass = pass
+    if @user
+      mail :to => @user.email, :subject => "Authorly - You are invited!"
+    end
+  end
+
   def ios_compilation_completion_notification(to, index_url, ipa_url)
     @index_url = index_url
     @ipa_url   = ipa_url
