7ea954a5326584dbd30b1f0b298706b3bea640ed
Chris Whitman <christopherscottwhitman@gmail.com>
2013-09-03 12:48:12 -0700

    #865 Remove http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   include Interapptive::Helpers::AuthorizationHelper
 
   protect_from_forgery
-  before_filter :http_authorize
   before_filter :authorize
   # force_ssl unless Rails.env.test?
 
@@ -16,14 +15,6 @@ class ApplicationController < ActionController::Base
 
   def signed_in?
     true if current_user
-  end
-
-  def http_authorize
-    if Rails.env.production? || Rails.env.staging?
-      authenticate_or_request_with_http_basic do |username, password|
-        username == "authorly" && password == "4u7h9R1y9"
-      end
-    end
   end
 
   def authorize
