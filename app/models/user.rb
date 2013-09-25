1fe038e63584bfe5ea12d0d3570530f0574b8a87
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-09-26 04:43:34 +0530

    (#947) Use resque to send forgot password emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ActiveRecord::Base
     generate_token :password_reset_token
     self.password_reset_sent_at = Time.zone.now
     save!
-    UserMailer.password_reset(self).deliver
+    Resque.enqueue(MailerQueue, 'UserMailer', 'password_reset', self.id)
   end
 
   def reset_password
