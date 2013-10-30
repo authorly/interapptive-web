1d02802c26c723c4e72ae6df7649e4d8a73326a6
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-10-30 15:18:29 +0530

    (#802) Application icons for Android.

--- app/models/android_storybook_application.rb
+++ app/models/android_storybook_application.rb
@@ -2,15 +2,24 @@ require 'open3'
 
 class AndroidStorybookApplication < AbstractStorybookApplication
   CRUCIBLE_ANDROID_DIR = File.join(Rails.root, '../../Crucible/HelloWorld/android/')
+  CRUCIBLE_ANDROID_RES_DIR = File.join(CRUCIBLE_ANDROID_DIR, 'res')
+  ANDROID_ICON_DIRECTORIES = {
+    :app_icon_32_32 => 'drawable-ldpi',
+    :app_icon_48_48 => 'drawable-mdpi',
+    :app_icon_72_72 => 'drawable-hdpi',
+    :app_icon_96_96 => 'drawable-xhdpi'
+  }
 
   def compile
     download_files_and_sanitize_json(ActiveSupport::JSON.decode(@json))
+    download_icons
     logger.info "Going to compile application with json:\n\n"
     logger.info @json_hash.inspect
     write_json_file
     #move_unused_files_out_of_compilation
     build_application
     #move_unused_files_to_resources
+    move_default_icons_to_android_res
     @json_hash
   end
 
@@ -27,6 +36,12 @@ class AndroidStorybookApplication < AbstractStorybookApplication
     Resque.enqueue(MailerQueue, 'UserMailer', 'android_compilation_completion_notification', recipient_email, @storybook.android_application.url)
   end
 
+  def move_default_icons_to_android_res
+    if @storybook.icon.present?
+      move_default_icons(CRUCIBLE_TMP_RESOURCES_DIR, CRUCIBLE_ANDROID_RES_DIR)
+    end
+  end
+
   private
 
   def build_application
@@ -36,5 +51,25 @@ class AndroidStorybookApplication < AbstractStorybookApplication
       logger.info e.read.chomp
     end
     logger.info("Done building android application for storybook: #{@storybook.id}")
+  end
+
+  def save_icon_files
+    ANDROID_ICON_DIRECTORIES.each do |accessor, name|
+      File.open(File.join(CRUCIBLE_ANDROID_RES_DIR, name, 'icon.png'), 'wb+') do |icon|
+        open(@storybook.icon.app_icon.send(accessor).url, 'rb') do |read_file|
+          icon << read_file.read
+        end
+      end
+    end
+  end
+
+  def move_default_icons_to_tmp
+    move_default_icons(CRUCIBLE_ANDROID_RES_DIR, CRUCIBLE_TMP_RESOURCES_DIR)
+  end
+
+  def move_default_icons(from_dir, to_dir)
+    ANDROID_ICON_DIRECTORIES.values.each do |name|
+      FileUtils.mv(File.join(from_dir, name, 'icon.png'), File.join(to_dir, name))
+    end
   end
 end
