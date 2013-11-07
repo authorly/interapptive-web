c0cbb5afab4009b41a5855b7e4890ac61a3de9b5
Irina Dumitrascu <me@dira.ro>
2013-11-07 21:57:22 +0200

    Fix tests #1081
    Remove sound uploader spec #1081
    
    duration is set by the zencoder transcoding process
    
    Update sound spec to reflect different json responses #1081
    
    depending on the transcoding status

--- spec/models/sound_spec.rb
+++ spec/models/sound_spec.rb
@@ -1,28 +1,36 @@
 require 'spec_helper'
 
 describe Sound do
-  let!(:sound) { Factory(:sound) }
+  let!(:sound) { Factory(:sound, sound: File.open(Rails.root.join("spec/factories/sounds/voicemail_received.wav"))) }
 
   context "#as_jquery_upload_response" do
-    it 'should be valid response' do
-      # Following chokes with image returned from Factory(:sound)
-      s = Sound.new
-      s.sound = File.open(Rails.root.join("spec/factories/sounds/voicemail_received.wav"))
-      s.save!
-
-      response = {
-        'id'          => s.id,
-        'name'        => s.read_attribute(:sound),
-        'size'        => s.sound.size,
-        'url'         => s.sound.url,
-        'delete_url'  => "/sounds/#{s.id}",
-        'delete_type' => 'DELETE',
-        'created_at'  => s.created_at,
-        'duration'    => nil,
-        'type'        => 'Sound',
+    let(:json) {
+      {
+        'id'                 => sound.id,
+        'name'               => sound.read_attribute(:sound),
+        'size'               => sound.sound.size,
+        'url'                => sound.sound.url,
+        'delete_url'         => "/sounds/#{sound.id}",
+        'delete_type'        => 'DELETE',
+        'created_at'         => sound.created_at,
+        'type'               => 'Sound',
+        'transcode_complete' => false,
       }
+    }
 
-      s.as_jquery_upload_response.should eql(response)
+    it 'should be valid response' do
+      sound.as_jquery_upload_response.should eql(json)
+    end
+
+    it 'should contain more details after transcoding' do
+      sound.meta_info[:response] = {'job' => {'state' => 'finished' }, 'input' => {'duration_in_ms' => 23000}}
+      expected = json.merge({
+        'mp3url'             => "http://authorly-test.s3.amazonaws.com/sounds/#{sound.id}/mp3_voicemail_received.mp3",
+        'oggurl'             => "http://authorly-test.s3.amazonaws.com/sounds/#{sound.id}/ogg_voicemail_received.ogg",
+        'duration'           => 23,
+        'transcode_complete' => true,
+      })
+      sound.as_jquery_upload_response.should eql(expected)
     end
   end
 end
