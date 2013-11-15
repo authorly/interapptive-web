26db8112feec8070d678a1568b6ebdd55c5db812
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-11-15 18:21:50 +0530

    (#1110) Display created at of a user in EST.

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,9 @@ module Interapptive
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+
+    # NOTE: Do not forget to change zone in PS for time_formats initializer when you change this.
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
