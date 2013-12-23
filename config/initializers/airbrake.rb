52d5c172094de468d4d601079ed37f8b71360ccb
Waseem Ahmad <talk.to.waseem@gmail.com>
2013-12-23 21:04:06 +0530

    (#1152) Use single api key. Airbrake will group errors with environment.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,3 @@
 Airbrake.configure do |config|
-  if Rails.env == 'production'
-    config.api_key = '7e87edd4d0698fdaad610b5d7cc4d28d'
-  else
-    config.api_key = '42970e138183dde448948c66e45b4f83'
-  end
+  config.api_key = '7e87edd4d0698fdaad610b5d7cc4d28d'
 end
