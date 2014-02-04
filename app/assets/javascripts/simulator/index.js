e4ca5e341e6b11e0d7133e2b9b838dc9f61b3f1b
Irina Dumitrascu <me@dira.ro>
2014-02-04 23:45:04 +0200

    Fix assets precompilation for cocos2d upgrade #1235

--- app/assets/javascripts/simulator/index.js
+++ app/assets/javascripts/simulator/index.js
@@ -10,8 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require cocos2d-html5
-//= require ../cocos2d_fixes
 //= require hamlcoffee
 //= require jquery
 //= require jquery_ujs
