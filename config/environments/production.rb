a5aaac09eb19a9b6595870051d2221264a1c06ba
Irina Dumitrascu <me@dira.ro>
2014-04-21 23:28:30 +0300

    Remove Airbrake.js #1372

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Interapptive::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(simulator.js administration.js detectmobilebrowser.js jquery.js user_sessions.js Cocos2d-html5-v2.2.2.min.js builder.js init_storybook.js airbrake-shim.js)
+  config.assets.precompile += %w(simulator.js administration.js detectmobilebrowser.js jquery.js user_sessions.js Cocos2d-html5-v2.2.2.min.js builder.js init_storybook.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
