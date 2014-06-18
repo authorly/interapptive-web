28a1a1cb1d47d8d643b071f4061fe684f09e5a3f
Waseem Ahmad <talk.to.waseem@gmail.com>
2014-06-18 22:49:54 +0530

    (#1407) User who signed ip within last 24hrs should have different created at time format display.

--- app/helpers/admin/subscription_user_helper.rb
+++ app/helpers/admin/subscription_user_helper.rb
@@ -4,5 +4,10 @@ module Admin
       return 'n/a' unless last_sign_in_at
       time_ago_in_words(last_sign_in_at) + 'ago'
     end
+
+    def sign_up_time(date_time)
+      return date_time.to_s(:us) if date_time > 24.hours.ago
+      time_ago_in_words(date_time) + "ago"
+    end
   end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,4 @@
+# Check out http://www.ruby-doc.org/core-2.1.2/Time.html#method-i-strftime for format directives
+
 Time::DATE_FORMATS[:authorly] = "%a, %b %e %Y %H:%M EST"
+Time::DATE_FORMATS[:us]       = "%m/%d/%Y"
