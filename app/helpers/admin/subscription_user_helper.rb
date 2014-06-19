d1e7e5d04a1b18068f491515adfdd276b51f901f
Waseem Ahmad <talk.to.waseem@gmail.com>
2014-06-19 05:38:34 +0530

    (#1407) Format the time display.

--- app/helpers/admin/subscription_user_helper.rb
+++ app/helpers/admin/subscription_user_helper.rb
@@ -2,11 +2,11 @@ module Admin
   module SubscriptionUserHelper
     def last_signed_in_mobile_at(last_sign_in_at)
       return 'n/a' unless last_sign_in_at
-      time_ago_in_words(last_sign_in_at) + 'ago'
+      time_ago_in_words(last_sign_in_at) + ' ago'
     end
 
     def sign_up_time(date_time)
-      return time_ago_in_words(date_time) + "ago" if date_time > 24.hours.ago
+      return time_ago_in_words(date_time) + " ago" if date_time > 24.hours.ago
       date_time.to_s(:us)
     end
   end
